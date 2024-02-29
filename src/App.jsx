63ec7320802a8e087de05aae6d077ebb7a7972a6
Pranesh98 <praneshchy23@gmail.com>
2024-02-29 21:30:59 +0600

    header section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'  
-import Cosmetics from './components/Cosmetics/Cosmetics'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <div className='app'>
-      <Cosmetics></Cosmetics>
+      <Header></Header>
     </div>
   )
 }
